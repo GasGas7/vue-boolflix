92d884fd4d0b160e126d5ca3592c834921451bde
GasGas7 <79295810+GasGas7@users.noreply.github.com>
2021-06-09 11:27:47 +0200

    Milestone_3 - Initialization

---
+++ Milestone_3/assets/js/main.js
@@ -0,0 +1,37 @@
+const app = new Vue ({
+
+el:'#app',
+
+data: {
+    urlfilms:"https://api.themoviedb.org/3/search/movie?api_key=f731de4dc238375a909e9a43cf8dd2da&query=",
+    urlserie:"https://api.themoviedb.org/3/search/tv?api_key=f731de4dc238375a909e9a43cf8dd2da&query=",
+    query:"",
+    films:[],
+    series:[],
+},
+
+methods:{
+    searchFilms(){
+        this.films=null;
+        axios
+            .get(this.urlfilms + this.query)
+            .then(resp => {
+            console.log(resp.data.results)
+            this.films=resp.data.results;
+        }).catch(e => {
+            console.error(e);
+            alert("La chiamata all'API non è andata a buon fine, riprova più tardi "+ e)
+        })
+        this.series=null;
+        axios
+            .get(this.urlserie + this.query)
+            .then(resp => {
+            console.log(resp.data.results)
+            this.series=resp.data.results;
+        }).catch(e => {
+            console.error(e);
+            alert("La chiamata all'API non è andata a buon fine, riprova più tardi. "+ e)
+        })
+    }
+}
+})
